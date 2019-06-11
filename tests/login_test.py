fae771b8bfb85208f4be61da722f2eefeae61e08
Snehal-kb <snehalkb@motax-solutions.com>
2019-06-11 15:56:54 +0530

    merge reports

--- tests/login_test.py
+++ tests/login_test.py
@@ -31,7 +31,7 @@ class TestLogin:
             print("Assertion error occurred.")
             print(error)
 
-            time = moment.now().strftime("%H-%M-%S_%m-%d-%Y")
+            time = moment.now().strftime("%m-%d-%Y_%H-%M-%S")
             functionName = utils.whoIam()
             ScreenshotName= functionName+"_"+time
             allure.attach(self.driver.get_screenshot_as_png(),name=ScreenshotName,attachment_type=allure.attachment_type.PNG)
